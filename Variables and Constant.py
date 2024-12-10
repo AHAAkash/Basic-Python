0052b28531d5143eef9684a3a41138f91a4cf834
Ahad Hosen Akash <162029494+AHAAkash@users.noreply.github.com>
2024-12-11 00:39:33 +0600

    Create Variables and Constant.py

---
+++ Variables and Constant.py
@@ -0,0 +1,22 @@
+#Declaring and assigning value to constant
+PI=3.14
+GRAVITY=9.18
+
+#Declaring and assigning value to variable
+a="Apple"
+print(a)
+
+# Changing value of a variable
+a="Orange"
+print(a);
+
+a=100
+print(a)
+
+#Assigning multiple values in a variable
+b, c, d = 2,2.5,"Akash"
+print(b,c,d)
+
+#Assigning same value in multiple variable
+a=b=c=5
+print(a,b,c)
