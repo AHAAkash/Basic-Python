fee657bb5b0a0eb7e1c4ac7a9d0561769dd55a5f
Ahad Hosen Akash <162029494+AHAAkash@users.noreply.github.com>
2024-12-31 12:37:07 +0600

    Create problem01.py

---
+++ Problem set/problem01.py
@@ -0,0 +1,14 @@
+#Problem 01 - printing Multi-String
+# """ print twinkle teinkle poem """
+print(''' Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!
+Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are ''')
