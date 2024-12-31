5677f7c763d699b42cf4883c2a93f25282c78144
Ahad Hosen Akash <162029494+AHAAkash@users.noreply.github.com>
2024-12-31 12:47:17 +0600

    Create problem02.py

---
+++ Problem set/problem02.py
@@ -0,0 +1,36 @@
+#using REPL --> Read Evaluate Print Loop 
+#printing the table of 5
+
+
+
+Microsoft Windows [Version 10.0.19045.5247]
+(c) Microsoft Corporation. All rights reserved.
+
+C:\Users\Akash>python
+Python 3.11.7 (main, Dec  7 2023, 09:09:57)  [GCC UCRT 13.2.0 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 5+6
+11
+>>> 5*1
+5
+>>> 5*2
+10
+>>> 5*3
+15
+>>> 5*4
+20
+>>> 5*5
+25
+>>> 5*6
+30
+>>> 5*6
+30
+>>> 5*7
+35
+>>> 5*8
+40
+>>> 5*9
+45
+>>> 5*10
+50
+>>>
