aa90b301db63cf394ad06a67d8ccfe811ab989bc
Ahad Hosen Akash <162029494+AHAAkash@users.noreply.github.com>
2025-01-03 10:27:09 +0600

    Add files via upload

---
+++ Problem set/problem05.py
@@ -0,0 +1,6 @@
+# Write a python program to find a remainder when the number is divided by z. and 
+# find grater than of two number
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("Remainder when a is divided by b is:",a%b)
+print("a is grater than of b!, is",a>b)
