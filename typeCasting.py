2be1406e1b76d9a6be29194b3068bd66c372c29e
Ahad Hosen Akash <162029494+AHAAkash@users.noreply.github.com>
2025-01-03 10:27:41 +0600

    Add files via upload

---
+++ typeCasting.py
@@ -0,0 +1,9 @@
+#Type Casting--->>>
+inte = 55
+print(type(inte))    #in this way we can find the datatype of a variable
+b= float(inte)       # and in this way we can typecastiing the variable
+b= str(inte)          #int to string........
+print(type(b))
+# e="Akash"
+# t= int(e)
+# print(type(t))
