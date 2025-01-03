5b79b2335c58a784e8e1a90b1df243dd5274265b
Ahad Hosen Akash <162029494+AHAAkash@users.noreply.github.com>
2025-01-03 10:26:45 +0600

    Add files via upload

---
+++ Problem set/problem04.py
@@ -0,0 +1,5 @@
+# Write a program to add 2 numbers and find average
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("The sum of a+b:",a+b)
+print("The average of these teo number is:",(a+b)/2)
