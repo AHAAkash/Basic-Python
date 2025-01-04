2739b59eb6754d7d747eeb48dd630347bfeaffdd
Ahad Hosen Akash <162029494+AHAAkash@users.noreply.github.com>
2025-01-04 20:33:29 +0600

    Add files via upload

---
+++ Problem set/problem07.py
@@ -0,0 +1,9 @@
+#Convert Binary,Octal,Hexadecimal number to Decimal
+print("Decimal of 1011 is:",0b1011)
+print("Decimal of 23 is:",0o23)
+print("Deciaml of AB is:",0xAB)
+
+#Output:
+# Decimal of 1011 is: 11
+# Decimal of 23 is: 19
+# Deciaml of AB is: 171
