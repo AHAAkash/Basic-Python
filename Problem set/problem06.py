15d790f359bb81d2349eed96129a4a5728dc46d0
Ahad Hosen Akash <162029494+AHAAkash@users.noreply.github.com>
2025-01-04 20:33:53 +0600

    Add files via upload

---
+++ Problem set/problem06.py
@@ -0,0 +1,6 @@
+#Write a program to find a square a number
+#In python we use ** to find square not like (a^2)
+a=int(input("Enter your number:"))
+print("Square of your enterned number is:",a**2)
+#another way is:
+                #print("Square of your enterned number is:",a*a)
